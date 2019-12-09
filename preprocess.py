1888e2edbd8626d4b5342a7bd6e0e6b27312b5b7
Xiaomeng Jin <xiaomengjin@DN0a1ea5a9.SUNet>
2019-12-08 20:36:53 -0800

    m

--- preprocess.py
+++ preprocess.py
@@ -357,6 +357,224 @@ print('Number of features using previous loans from other institutions data: ', 
 # Align the dataframes, this will remove the 'TARGET' column
 # app_train_poly, test = app_train_poly.align(test, join = 'inner', axis = 1)
 
+def agg_numeric_new(df, parent_var, df_name):
+    """
+    Groups and aggregates the numeric values in a child dataframe
+    by the parent variable.
+    
+    Parameters
+    --------
+        df (dataframe): 
+            the child dataframe to calculate the statistics on
+        parent_var (string): 
+            the parent variable used for grouping and aggregating
+        df_name (string): 
+            the variable used to rename the columns
+        
+    Return
+    --------
+        agg (dataframe): 
+            a dataframe with the statistics aggregated by the `parent_var` for 
+            all numeric columns. Each observation of the parent variable will have 
+            one row in the dataframe with the parent variable as the index. 
+            The columns are also renamed using the `df_name`. Columns with all duplicate
+            values are removed. 
+    
+    """
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != parent_var and 'SK_ID' in col:
+            df = df.drop(columns = col)
+            
+    # Only want the numeric variables
+    parent_ids = df[parent_var].copy()
+    numeric_df = df.select_dtypes('number').copy()
+    numeric_df[parent_var] = parent_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(parent_var).agg(['count', 'mean', 'max', 'min', 'sum'])
+
+    # Need to create new column names
+    columns = []
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        if var != parent_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+    
+    agg.columns = columns
+    
+    # Remove the columns with all redundant values
+    _, idx = np.unique(agg, axis = 1, return_index=True)
+    agg = agg.iloc[:, idx]
+
+    return agg
+
+
+def agg_categorical_new(df, parent_var, df_name):
+    """
+    Aggregates the categorical features in a child dataframe
+    for each observation of the parent variable.
+    
+    Parameters
+    --------
+    df : dataframe 
+        The dataframe to calculate the value counts for.
+        
+    parent_var : string
+        The variable by which to group and aggregate the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+        
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+    
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with aggregated statistics for each observation of the parent_var
+        The columns are also renamed and columns with duplicate values are removed.
+        
+    """
+    
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('category'))
+    # Make sure to put the identifying id on the column
+    categorical[parent_var] = df[parent_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(parent_var).agg(['sum', 'count', 'mean'])
+    
+    column_names = []
+    
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['sum', 'count', 'mean']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+    
+    categorical.columns = column_names
+    
+    # Remove duplicate columns by values
+    _, idx = np.unique(categorical, axis = 1, return_index = True)
+    categorical = categorical.iloc[:, idx]
+    
+    return categorical
+
+
+def aggregate_client(df, group_vars, df_names):
+    """Aggregate a dataframe with data at the loan level 
+    at the client level
+    
+    Args:
+        df (dataframe): data at the loan level
+        group_vars (list of two strings): grouping variables for the loan 
+        and then the client (example ['SK_ID_PREV', 'SK_ID_CURR'])
+        names (list of two strings): names to call the resulting columns
+        (example ['cash', 'client'])
+        
+    Returns:
+        df_client (dataframe): aggregated numeric stats at the client level. 
+        Each client will have a single row with all the numeric data aggregated
+    """
+    
+    # Aggregate the numeric columns
+    df_agg = agg_numeric_new(df, parent_var = group_vars[0], df_name = df_names[0])
+    
+    # If there are categorical variables
+    if any(df.dtypes == 'category'):
+        # Count the categorical columns
+        df_counts = agg_categorical_new(df, parent_var = group_vars[0], df_name = df_names[0])
+
+        # Merge the numeric and categorical
+        df_by_loan = df_counts.merge(df_agg, on = group_vars[0], how = 'outer')
+
+        # Merge to get the client id in dataframe
+        df_by_loan = df_by_loan.merge(df[[group_vars[0], group_vars[1]]], on = group_vars[0], how = 'left')
+
+        # Remove the loan id
+        df_by_loan = df_by_loan.drop(columns = [group_vars[0]])
+
+        # Aggregate numeric stats by column
+        df_by_client = agg_numeric_new(df_by_loan, parent_var = group_vars[1], df_name = df_names[1])
+
+        
+    # No categorical variables
+    else:
+        # Merge to get the client id in dataframe
+        df_by_loan = df_agg.merge(df[[group_vars[0], group_vars[1]]], on = group_vars[0], how = 'left')
+        
+        # Remove the loan id
+        df_by_loan = df_by_loan.drop(columns = [group_vars[0]])
+        
+        # Aggregate numeric stats by column
+        df_by_client = agg_numeric_new(df_by_loan, parent_var = group_vars[1], df_name = df_names[1])
+        
+    # Memory management
+
+    return df_by_client
+
+def convert_types(df, print_info = False):
+    
+    original_memory = df.memory_usage().sum()
+    
+    # Iterate through each column
+    for c in df:
+        
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)
+            
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')
+        
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)
+        
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)
+            
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+        
+    new_memory = df.memory_usage().sum()
+    
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+        
+    return df
+
+app_train_poly = convert_types(app_train_poly, print_info=True)
+cash = pd.read_csv('../input/POS_CASH_balance.csv')
+cash = convert_types(cash, print_info=True)
+cash_by_client = aggregate_client(cash, group_vars = ['SK_ID_PREV', 'SK_ID_CURR'], df_names = ['cash', 'client'])
+
+app_train_poly = app_train_poly.merge(cash_by_client, on = 'SK_ID_CURR', how = 'left')
+
+
+
+
+# credit = pd.read_csv('../input/credit_card_balance.csv')
+# credit = convert_types(credit, print_info = True)
+# credit_by_client = aggregate_client(credit, group_vars = ['SK_ID_PREV', 'SK_ID_CURR'], df_names = ['credit', 'client'])
+
+# app_train_poly = app_train_poly.merge(credit_by_client, on = 'SK_ID_CURR', how = 'left')
+
+# installments = pd.read_csv('../input/installments_payments.csv')
+# installments = convert_types(installments, print_info = True)
+
+# installments_by_client = aggregate_client(installments, group_vars = ['SK_ID_PREV', 'SK_ID_CURR'], df_names = ['installments', 'client'])
+
+# app_train_poly = app_train_poly.merge(installments_by_client, on = 'SK_ID_CURR', how = 'left')
 
 
 
